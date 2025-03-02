aff48b64345fbf9bc61d4fe5b7d5eed4d981c6e5
Jordy <jordy@nefkon.com>
2025-03-02 11:40:43 +0100

    rps contracts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,19 +4,9 @@ import {Script, console} from "forge-std/Script.sol";
 
 import {GameContract} from "../src/Game.sol";
 
-// First step in process
-// Generate new monad-deployer account
-// cast wallet import monad-deployer --private-key $(cast wallet new | grep 'Private key:' | awk '{print $3}')
-// print wallet address
-// cast wallet address --account monad-deployer # 0x73673Baa430f20d45dA0d14073988d9F22db1C23
-
-
-// Local forge script script/Deploy.s.sol:Deploy --sender 0x73673Baa430f20d45dA0d14073988d9F22db1C23 --rpc-url $RPC_URL_LOCAL --account monad-deployer --broadcast
-// Sepolia forge script script/Deploy.s.sol:Deploy --rpc-url $RPC_URL_DEV --account monad-deployer --broadcast
-// Prd forge script script/Deploy.s.sol:Deploy --rpc-url $RPC_URL_PRD --broadcast
 contract Deploy is Script {
-    address public teamVault = address(0x7B52aC329297D8172102776d6956b7f26A00e7BA); // Firefox account 1
-    address public gameManager = address(0x8427941818410c2519cbb294C70370458145ee64); // Firefox account 5
+    address public teamVault = address(0x7B52aC329297D8172102776d6956b7f26A00e7BA);
+    address public gameManager = address(0x8427941818410c2519cbb294C70370458145ee64);
 
     address public deployer = address(0x73673Baa430f20d45dA0d14073988d9F22db1C23);
     function run() public {
@@ -29,10 +19,15 @@ contract Deploy is Script {
         vm.startBroadcast();
         GameContract game = new GameContract(teamVault, gameManager);
         console.log("GameContract deployed at: ", address(game));
+        console.log("TEAM_VAULT: ", game.TEAM_VAULT());
+        console.log("GAME_MANAGER: ", game.GAME_MANAGER());
         vm.stopBroadcast();
     }
 
     function _local() private {
+        teamVault = address(0xa0Ee7A142d267C1f36714E4a8F75612F20a79720); // Foundry9
+        gameManager = address(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f); // Foundry8
+
         uint256 account9 = vm.envUint("PRIVATE_KEY_9");
         vm.startBroadcast(account9);
         (bool success,) = deployer.call{value: 1 ether}("");
